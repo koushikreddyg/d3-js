ec53ece7527d6a7300ef058a419e0688112af39e
Koushik Reddy <koushikreddygunapati@Koushiks-MacBook-Pro.local>
2019-05-08 17:31:38 -0500

    bar chart attr tweens

--- src/BarChart.js
+++ src/BarChart.js
@@ -14,6 +14,8 @@ const totalHeight = 650;
 const width = 450;
 const height = 500;
 
+
+
 class App extends React.Component {
 
   componentDidMount() {
@@ -34,7 +36,7 @@ class App extends React.Component {
     // add a linear scale for the y axis and assign value to it and max height
     const y = d3.scaleLinear()
       .domain([0, d3.max(data, d => d.value)])
-      .range([height,0])
+      .range([height, 0])
     // add a band width and assign name to it and padding and max width
     const x = d3.scaleBand()
       .domain(data.map(item => item.name))
@@ -52,18 +54,51 @@ class App extends React.Component {
     // add a y axis group and assign all the values in domain and call all the values in xaxis
     bar.append("g").call(d3.axisLeft(y))
 
+    const t = d3.transition()
+      .duration(500)
+
+      const widthTween = (d) => {
+        const i=d3.interpolate(0, x.bandwidth())
+        return (t)=>i(t)
+      }
+
+      // const heightTween=(d)=>{
+      //   const i= height - y(d.value)
+      //   return (t)=>i(t)
+
+      // }
+
 
 
     // add rects to the bar and set the width of each bar and height of each bar
     // set the distance from x axis
     // set the inverse height and push the div to the rest of the height
-    bar.selectAll("rect").data(data).enter()
-      .append('rect')
+
+    const rects = bar.selectAll("rect").data(data);
+    rects.selectAll("rect").remove()
+
+    rects
       .attr('width', d => x.bandwidth(d))
-      .attr("height", d => height - y(d.value))
       .attr('fill', 'orange')
       .attr('x', (d) => x(d.name))
+    // .transition(t)
+    // .attr('y', d => y(d.value))
+    // .attr("height", d => height - y(d.value))
+
+
+    rects.enter()
+      .append('rect')
+      .attr('width', 0)
+      .attr("height", 0)
+      .attr("y", height)
+      .attr('fill', 'orange')
+      .attr('x', (d) => x(d.name))
+      // .merge()
+      .transition(t)
+      .attrTween("width", widthTween)
       .attr('y', d => y(d.value))
+      .attr("height", d => height - y(d.value))
+
 
   }
 
